0d8f05ff4ad94be819f26a987c5c6954f21f0386
unknown <judith.hantisse@telecom-sudparis.eu>
2019-07-06 02:00:06 +0200

    Commentaires MobAttackBehaviour.h

--- MicroProjet/MobAttackBehaviour.h
+++ MicroProjet/MobAttackBehaviour.h
@@ -11,13 +11,22 @@ public:
 	virtual ~MobAttackBehaviour();
 
 	bool isActive(Mob& mob);
+	//called in the AttackingMobState class
 	virtual void activeAttackBox(Mob& mob, bool active);
+
+	//called in the AttackingMobState function
 	virtual void attack(Mob const& mob) = 0;
+
+	//called in the mob update function
 	virtual void update(sf::Time dt);
+
+	//called in the mob createBody function
 	virtual void createBody(Mob& mob);
 
+	//called in the mob draw function
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
+	//to sort the vector of MobAttackBehaviours : behaviours with the lower attack distance have priority
 	bool operator<(MobAttackBehaviour const& behaviour);
 
 	int getAttackDistance() const;
@@ -31,6 +40,7 @@ protected:
 	int m_attackRate;
 	int m_attackTiming;
 
+	//vector of projectiles the mob fired
 	std::vector<std::unique_ptr<Projectile>> m_projectiles;
 
 
